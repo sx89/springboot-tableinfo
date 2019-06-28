fe16d9cc47e2565948a29de3c90c0a8af82f483f
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-06-28 20:02:17 +0800

    整理

--- src/main/java/com/sx/table/core/dao/nativesql/NativeSqlFromEM.java
+++ src/main/java/com/sx/table/core/dao/nativesql/NativeSqlFromEM.java
@@ -10,16 +10,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * 技术选择基本思路:
+ * 技术选择思路:
  * <p>
  * 使用原生sql做desc table;是因为jpa在对 "desc ?1"注入tablename的时候;会变成desc 'tablename'; 带了单引号导致语法错误
  * <p>
  * jpa对多表join并不灵活,需要在查的表的实体类内部做关联,不适合任意表之间join
  * <p>
  * 不使用类来存储信息而是HashMap;是因为类有哪些变量需要根据具体的表有哪些字段来定制,而HashMap可以根据不同的表的字段数动态扩张
+ * 后期会有一百多张表,每一个表创建一个实体类也不太现实,不如每个表对应一个HashMap
  * <p>
  * 使用mycat做中间件,原生sql做join查询
  *
+ * 参考博客:https://www.cnblogs.com/zhouguanglin/p/7625655.html
+ * 
  * @author sx
  */
 @Service
